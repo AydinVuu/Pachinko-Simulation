872736246f2e9c6921c431d61cad10271a1977a1
AydinVuu <90784581+AydinVuu@users.noreply.github.com>
2024-04-04 20:23:47 -0400

    Update BallFallKO.py

--- BallFallKO.py
+++ BallFallKO.py
@@ -167,7 +167,7 @@ def Collision_detection(activeballs, bigball, triangle):
 
                     if triangle.rightedge[j][0] == round(ball.position[0] + (radius * math.cos(triangle.rangle))):
 
-                        if triangle.rightedge[j][1] < (ball.position[1] - (radius * math.sin(triangle.rangle))) and ((ball.position[1] - (radius* math.sin(triangle.langle))) < triangle.position[2][1]):
+                        if triangle.rightedge[j][1] < (ball.position[1] - (radius * math.sin(triangle.rangle))) and ((ball.position[1] - (radius* math.sin(triangle.langle))) < triangle.pos[2][1]):
 
                             dis = triangle.leftedge[j][1] - (ball.position[1] - (radius + math.sin(triangle.rangle)))
                             ball.position[1] -= dis
@@ -178,13 +178,12 @@ def Collision_detection(activeballs, bigball, triangle):
             elif ball.position[0] <= triangle.pos[0][0]: #left side
                 for k in range(len(triangle.leftedge)):
                     if triangle.leftedge[k][0] == round(ball.position[0] + (radius * math.cos(triangle.langle))):
-                        if (triangle.leftedge[k][1] < (ball.position[1] - (radius* math.sin(triangle.langle)))) and ((ball.position[1] - (radius* math.sin(triangle.langle))) < triangle.position[2][1]):
+                        if (triangle.leftedge[k][1] < (ball.position[1] - (radius* math.sin(triangle.langle)))) and ((ball.position[1] - (radius* math.sin(triangle.langle))) < triangle.pos[2][1]):
                             dis = triangle.leftedge[k][1] - (ball.position[1] - (radius + math.sin(triangle.langle)))
                             ball.position[1] -= dis 
                             initial_velocity = math.sqrt(ball.velocity[0]**2 + ball.velocity[1]**2)       
                             ball.velocity[0] = -COR * initial_velocity * math.cos(triangle.langle)
                             ball.velocity[1] = -COR * initial_velocity * math.sin(triangle.langle)
-
 
 
 
